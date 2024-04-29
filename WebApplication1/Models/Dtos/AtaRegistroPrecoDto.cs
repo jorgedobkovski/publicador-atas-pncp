9016f4e8e0f62dfe00ad77938d3979cc78150dee
jorge <dobkovskijorge@gmail.com>
2024-04-29 12:24:48 -0400

    feat: adicionando textos de display no dto

--- WebApplication1/Models/Dtos/AtaRegistroPrecoDto.cs
+++ WebApplication1/Models/Dtos/AtaRegistroPrecoDto.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models.Dtos
 {
     public class AtaRegistroPrecoDto
     {
+        [Display(Name = "Ano da contratação")]
         public string anoCompra { get; set; }
+        [Display(Name = "Sequencial da contratação")]
         public string sequencialCompra { get; set; }
+        [Display(Name = "Numero da ata de registro de preço")]
         public string numeroAta { get; set; }
+        [Display(Name = "Ano da ata de registro de preço")]
         public string anoAta { get; set; }
+        [Display(Name = "Data da assinatura")]
         public string dataAssinatura { get; set; }
+        [Display(Name = "Data inicial da vigência")]
         public DateTime dataInicioVigencia { get; set; }
+        [Display(Name = "Data final da vigência")]
         public DateTime dataFimVigencia { get; set; }
+        [Display(Name = "Anexo da ata de registro de preço")]
         public IFormFile arquivo { get; set; }
     }
 }
